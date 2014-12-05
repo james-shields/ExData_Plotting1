ac179eba2bf891e97fb333b043afadfda1e12745
james-shields <james.shields@gmail.com>
2014-12-05 07:20:31 -0500

    Added code to create a legend in the top right of the plot.

--- plot3.R
+++ plot3.R
@@ -15,9 +15,14 @@ png("./plot3.png")
 plot(x=data$DateTime, y=data$Sub_metering_1,type="n", yaxt="n", ylab="Energy Sub Metering", xlab="")
 axis(side=2, at=c(0, 10, 20, 30))
 
+# add data series one at a time using the lines function
 lines(x=data$DateTime, y=data$Sub_metering_1, col="Black")
 lines(x=data$DateTime, y=data$Sub_metering_2, col="Red")
 lines(x=data$DateTime, y=data$Sub_metering_3, col="Blue")
+
+# add a legend to the top right
+legend("topright", lty=c(1,1,1), col=c("Black", "Red", "Blue"),
+       legend=c("Sub metering 1", "Sub metering 2", "Sub metering 3"))
 
 #close the graphics device
 dev.off()
